9de3acf13bfe15deee562032f3b7309e94279af4
pipatte <pierre.patte21@orange.fr>
2026-07-10 21:06:26 +0200

    Added High Five and All for One

--- Pokemon 1048/Plugins/Pokémon 1048 Add-ons/011_Battle/003_Move/007_MoveEffects_BattlerOther.rb
+++ Pokemon 1048/Plugins/Pokémon 1048 Add-ons/011_Battle/003_Move/007_MoveEffects_BattlerOther.rb
@@ -62,3 +62,56 @@ class Battle::Move::AddRockTypeToTargetAndParalyze < Battle::Move
     target.pbParalyze(user) if target.pbCanParalyze?(user, nil, self)
   end
 end
+
+#===============================================================================
+# Increases the user's and target's Attack by 1 stage. (High Five)
+#===============================================================================
+class Battle::Move::RaiseUserAndTargetAtk1 < Battle::Move
+  def canMagicCoat?; return true; end
+
+  def pbMoveFailed?(user, targets)
+    return false if damagingMove?
+  end
+
+  def pbEffectAgainstTarget(user, target)
+    if target.pbCanRaiseStatStage?(:ATTACK, user, self)
+      target.pbRaiseStatStage(:ATTACK, 1, user)
+    end
+    if user.pbCanRaiseStatStage?(:ATTACK, user, self)
+      user.pbRaiseStatStage(:ATTACK, 1, user)
+    end
+  end
+end
+
+#===============================================================================
+# Loses 1/4 of max HP, switches and heals the next ally of the amount lost (All for One)
+#===============================================================================
+class Battle::Move::LoseHpAndSwitchAndHealNextAllyTotalHPLost < Battle::Move
+
+  def healingMove?;  return true; end
+  def canMagicCoat?; return true; end
+
+  def pbEndOfMoveUsageEffect(user, targets, numHits, switchedBattlers)
+    @subLife = [user.totalhp / 4, 1].max
+    user.pbReduceHP(@subLife, false, false)
+    return if user.fainted? || numHits == 0
+    return if !@battle.pbCanChooseNonActive?(user.index)
+    @battle.pbPursuit(user.index)
+    return if user.fainted?
+    newPkmn = @battle.pbGetReplacementPokemonIndex(user.index)   # Owner chooses
+    return if newPkmn < 0
+    @battle.pbRecallAndReplace(user.index, newPkmn, false, true)
+    @battle.pbClearChoice(user.index)   # Replacement Pokémon does nothing this round
+    @battle.moldBreaker = false
+    @battle.pbOnBattlerEnteringBattle(user.index)
+    switchedBattlers.push(user.index)
+    if user.hp == user.totalhp
+      @battle.pbDisplay(_INTL("{1}'s HP is full!", user.pbThis)) if show_message
+    elsif !user.canHeal?
+      @battle.pbDisplay(_INTL("{1} is unaffected!", user.pbThis)) if show_message
+    else
+      user.pbRecoverHP(@subLife)
+      @battle.pbDisplay(_INTL("{1}'s HP was restored.", user.pbThis))
+    end
+  end
+end
