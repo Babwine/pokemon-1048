357c4e3cc74b334c386d795e5d3590eef2228a74
pipatte <pierre.patte21@orange.fr>
2026-07-10 20:29:16 +0200

    Added Goldfinger and Petrifaction

--- Pokemon 1048/Plugins/Pokémon 1048 Add-ons/011_Battle/003_Move/007_MoveEffects_BattlerOther.rb
+++ Pokemon 1048/Plugins/Pokémon 1048 Add-ons/011_Battle/003_Move/007_MoveEffects_BattlerOther.rb
@@ -19,3 +19,46 @@ class Battle::Move::DeafenTarget < Battle::Move
     target.pbDeafen if target.pbCanDeafen?(user, true, self)
   end
 end
+
+#===============================================================================
+# Gives target the Steel type. (Iron Filings)
+#===============================================================================
+class Battle::Move::AddSteelTypeToTarget < Battle::Move
+  def canMagicCoat?; return true; end
+
+  def pbFailsAgainstTarget?(user, target, show_message)
+    if !target.canChangeType? || !GameData::Type.exists?(:STEEL) || target.pbHasType?(:STEEL)
+      @battle.pbDisplay(_INTL("But it failed!")) if show_message
+      return true
+    end
+    return false
+  end
+
+  def pbEffectAgainstTarget(user, target)
+    target.effects[PBEffects::ExtraType] = :STEEL
+    typeName = GameData::Type.get(:STEEL).name
+    @battle.pbDisplay(_INTL("{1} transformed into the {2} type!", target.pbThis, typeName))
+  end
+end
+
+#===============================================================================
+# Gives target the Rock type and paralyzes it. (Petrifaction)
+#===============================================================================
+class Battle::Move::AddRockTypeToTargetAndParalyze < Battle::Move
+  def canMagicCoat?; return true; end
+
+  def pbFailsAgainstTarget?(user, target, show_message)
+    if !target.canChangeType? || !GameData::Type.exists?(:ROCK) || target.pbHasType?(:ROCK)
+      @battle.pbDisplay(_INTL("But it failed!")) if show_message
+      return true
+    end
+    return false
+  end
+
+  def pbEffectAgainstTarget(user, target)
+    target.effects[PBEffects::ExtraType] = :ROCK
+    typeName = GameData::Type.get(:ROCK).name
+    @battle.pbDisplay(_INTL("{1} transformed into the {2} type!", target.pbThis, typeName))
+    target.pbParalyze(user) if target.pbCanParalyze?(user, nil, self)
+  end
+end
